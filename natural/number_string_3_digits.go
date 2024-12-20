3cff6c88eb99be9d50b517210fd9224816bc181b
Nareik2610 <Nareik.2610@gmx.de>
2024-12-20 15:38:59 +0100

    Aufgaben update

--- natural/number_string_3_digits.go
+++ natural/number_string_3_digits.go
@@ -3,5 +3,20 @@ package natural
 // NumberString3Digits erwartet eine Zahl 0 <= n <= 999 und liefert den zugehörigen String.
 func NumberString3Digits(n int) string {
 	// TODO
-	return ""
+
+	zahleneinsbisneunzehn := []string{"null", "Eins", "Zwei", "Drei", "Vier", "fünf", "sechs", "seiben", "acht", "neun", "zehn", "elf", "zwölf", "dreizehn", "vierzehn", "fünfzehn", "sechzehn", "siebzehn", "achtzehn", "neunzehn"}
+	zahlnull := "null"
+
+	if n == 0 {
+
+		return zahlnull
+
+	}
+	if n > 0 && n < 20 {
+
+		return zahleneinsbisneunzehn[n]
+	}
+
+	return zahlen[n]
+
 }
